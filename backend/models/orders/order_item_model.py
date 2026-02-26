ea0b8eb500aa52de11549103f281292d7c47d432
Brady Bracken <bradyryanbracken@gmail.com>
2026-02-26 07:52:50 -0800

    Add OrderItem class

---
+++ backend/models/orders/order_item_model.py
@@ -0,0 +1,26 @@
+# This file defines the OrderItem class, representing an order item within our system.
+
+from dataclasses import dataclass
+
+from backend.models.restaurant.menu_item_model import MenuItem
+
+@dataclass
+class OrderItem:
+
+    id: int
+    menu_item: MenuItem
+    quantity: int
+
+    def __post_init__(self) -> None:
+        """
+        This runs right after the dataclass constructor, which means it runs after we create an OrderItem object.
+        We use it to validate that the OrderItem object is not invalid.
+        """
+        if not isinstance(self.id, int) or self.id < 0:
+            raise ValueError("id must be a non-negative integer")
+
+        if not isinstance(self.menu_item, "MenuItem"):
+            raise ValueError("menu_item must be a MenuItem object")
+
+        if not isinstance(self.quantity, int) or self.quantity <= 0:
+            raise ValueError("quantity must be a positive integer")
