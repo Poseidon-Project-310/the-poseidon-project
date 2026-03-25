6813f078293d8b2abb3222782bae247ecce0e825
john_doe <john@example.com>
2026-03-25 10:56:31 -0700

    fix conftest

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -6,9 +6,8 @@ from decimal import Decimal
 from uuid import uuid4
 from unittest.mock import MagicMock
 from backend.schemas.items_schema import MenuItem as MenuItemSchema
-from backend.models.user.restaurant_owner_model import RestaurantOwner
-from backend.models.restaurant.restaurant_model import Restaurant
-from backend.models.restaurant.menu_item_model import MenuItem
+
+
 
 # add project root to import path
 ROOT = Path(__file__).resolve().parents[1]
@@ -27,47 +26,3 @@ def raw_menu_item_data():
 def sample_menu_item(raw_menu_item_data):
     return MenuItemSchema(**raw_menu_item_data)
 
-@pytest.fixture
-def owner():
-    """
-    Return real RestaurantOwner
-    """
-    return RestaurantOwner(
-        id=1,
-        username="John_Doe",
-        email="john_doe@gmail.com",
-        password_hash="SecurePass123"
-    )
-
-@pytest.fixture
-def mock_owner():
-    """
-    Return mock for tests where owner login is not important
-    """
-    mock = MagicMock(spec=RestaurantOwner)
-    mock.id = 99
-    mock.username = "MockUser"
-    return mock
-
-@pytest.fixture
-def sample_item():
-    """
-    return valid menu item for FR3
-    """
-    return MenuItem(
-        id=101,
-        name="Burger",
-        price=9.99,
-        tags=["Popular"])
-
-@pytest.fixture
-def restaurant(owner, sample_item):
-    """
-    Return default instance linked to owner
-    """
-    return Restaurant(
-        id=1,
-        name="John's Diner",
-        owner=owner,
-        menu=[sample_item]
-    )
