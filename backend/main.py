0b2a656c9d2922fcf85b04ab55fd4c74ba7abf7a
john_doe <john@example.com>
2026-04-07 12:09:00 -0700

    fixed error

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from backend.routes.search_routes import router as search_router
 from backend.routes.notifications_router import router as notification_router
 from backend.routes.payment_router import router as payment_router
 from backend.routes.delivery_router import router as delivery_router
+from backend.routes.items_routes import router as items_routes
 
 app = FastAPI(title="The Poseidon Project")
 
