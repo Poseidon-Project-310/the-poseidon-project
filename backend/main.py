1348bad10cc4cb66c64e1f938976e29fb5e179bf
john_doe <john@example.com>
2026-04-07 13:21:28 -0700

    Add CORS middleware configuration to FastAPI app

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from backend.routes import restaurant_router, search_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="The Poseidon Project")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(restaurant_router.router)
 app.include_router(search_routes.router)
